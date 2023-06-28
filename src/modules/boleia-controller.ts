0b91d9bd39420c5d2ecbb1a0781d006ada6130cf
AlvesBenvindo <80269548+AlvesBenvindo@users.noreply.github.com>
2023-06-28 23:48:20 +0100

    Update boleia-controller.ts

--- src/modules/boleia-controller.ts
+++ src/modules/boleia-controller.ts
@@ -18,8 +18,24 @@ export class BoleiaController {
     if(!record) throw new NotFoundError('Não foi possível criar esta boleia boleia');
     return response.status(200).json(record)
   }
+  
+  public async minhasBoleias(request: Request, response: Response){
+    const { cod_utente } = request.body;
+    const record = await new BoleiaRepositorio().boleiasCriadas(cod_utente);
+    if(!record) throw new NotFoundError('Ainda não criaste boleia');
+    return response.status(200).json(record);
+  }
+
+  public async boleiasInscritas(request: Request, response: Response){
+    const { cod_utente, cod_boleia } = request.body;
+    const record = await new BoleiaRepositorio().boleiasInscritas(cod_utente,cod_boleia);
+    if(!record) throw new NotFoundError('Ainda não inscriveste em nenhuma boleia');
+    return response.status(200).json(record);
+  }
 
   public initializeRoutes() {
     this.router.post(`${this.path}/create`,this.criarBoleia)
+    this.router.get(`${this.path}/my_rides`,this.minhasBoleias)
+    this.router.get(`${this.path}/my_records`,this.boleiasInscritas)
   }
 }
